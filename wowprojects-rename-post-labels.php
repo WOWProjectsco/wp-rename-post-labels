ec5654e6bc29854d8ad6cd467ecbad47f3680d46
XicoOfficial <xicoofficial@gmail.com>
2016-01-06 15:20:15 -0600

    Update wowprojects-rename-post-labels.php - Add basic tags and ifo

---
+++ wowprojects-rename-post-labels.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+* Plugin Name: Rename Post Labels
+* Plugin URI: http://wowdevshop.com
+* Description: This plugin let's you change the default labels of the post type and customize the icon.
+* Author: XicoOfficial
+* Version: 1.0.0
+* License: GPLv2
+* Author URI: http://wowdevshop.com
+* Text Domain: rename-post-labels-by-wowdevshop
+*
+* @package WordPress
+* @subpackage WowDevShop_Rename_Post_Labels
+* @author XicoOfficial
+* @since 1.0.0
+*/
