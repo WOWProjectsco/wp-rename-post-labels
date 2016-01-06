21a00dc893d63f584235ad559247be326acbae2f
XicoOfficial <xicoofficial@gmail.com>
2016-01-06 17:29:34 -0600

    Add support for changing the menu_icon

--- includes/menu-settings.php
+++ includes/menu-settings.php
@@ -5,8 +5,12 @@
             <form method="post" action="">
 				<?php settings_fields( 'disable-settings-group' ); ?>
             	<?php do_settings_sections( 'disable-settings-group' ); ?>
-			<br/>
 			<table>
+			<tr>
+				<td></td>
+				<td><label> <h4><?php _e('Change Post Labels') ?><h4> </label></td>
+				<td> </td>
+			</tr>
 			<tr>
 				<td><label> <?php _e('Posts') ?> </label></td>
 				<td><input type="text" size="25" name="name" value="<?php echo $name ?>" maxlength="20" /></td>
@@ -56,6 +60,16 @@
 				<td><label> <?php _e('No posts found in Trash.') ?> </label></td>
 				<td><input type="text" size="30" name="not_found_in_trash" value="<?php echo $not_found_in_trash ?>" maxlength="50" /> </td>
 				<td> </td>
+			</tr>
+			<tr>
+				<td></td>
+				<td>			<hr><label> <h4><?php _e('Change Icon') ?><h4> </label></td>
+				<td> </td>
+			</tr>
+			<tr>
+				<td><label> <?php _e('Custom Icon') ?> </label></td>
+				<td><input type="text" size="25" name="menu_icon" value="<?php echo $menu_icon ?>" maxlength="50" /> </td>
+				<td> *Check the available dashicons <a target="_blank" href="https://developer.wordpress.org/resource/dashicons">here</a>. </td>
 			</tr>
 			<table>
                 <?php submit_button('Save all changes', 'primary', 'submit', TRUE); ?>
--- wowprojects-rename-post-labels.php
+++ wowprojects-rename-post-labels.php
@@ -19,6 +19,13 @@
 // Register Subpage menu on settings
 add_action( 'admin_menu', 'wds_register_subpage' );
 
+// Change menu labels
+add_action( 'admin_menu', 'wds_change_admin_menu_labels' );
+
+// Change post labels
+add_action( 'init', 'wds_change_post_labels' );
+
+// Callback funtion that register the new subpage
 function wds_register_subpage() {
 	add_options_page( __('Rename Post Labels', 'RenamePostLabels'), __('Rename Post Labels', 'RenamePostLabels'), 'administrator', 'rename-post-labels', 'wds_RenamePostLabels');
 }
@@ -36,6 +43,7 @@ function wds_RenamePostLabels() {
 		$data['search_items'] = sanitize_text_field( $_POST['search_items'] );
 		$data['not_found'] = sanitize_text_field( $_POST['not_found'] );
 		$data['not_found_in_trash'] = sanitize_text_field( $_POST['not_found_in_trash'] );
+		$data['menu_icon'] = sanitize_text_field( $_POST['menu_icon']);
 		update_option('RenamePostLabels', $data);
 	}
 	else {
@@ -53,9 +61,53 @@ function wds_RenamePostLabels() {
 	$not_found = (isset($data['not_found']))? esc_html($data['not_found']) : __('No posts found', 'RenamePostLabels');
 	$not_found_in_trash = (isset($data['not_found_in_trash']))? esc_html($data['not_found_in_trash']) : __('No posts found in Trash', 'RenamePostLabels');
 
+	$menu_icon = (isset($data['menu_icon']))? esc_html($data['menu_icon']) : __('dashicons-admin-post', 'RenamePostLabels');
+
 	// Call the menu settings template
 	include( 'includes/menu-settings.php' );
 
+}
+
+
+// Callback funtion that changes the admin menu labels
+function wds_change_admin_menu_labels() {
+    global $menu;
+    global $submenu;
+	$data =  get_option('RenamePostLabels');
+	if(current_user_can( 'edit_posts' )){
+		if($data){
+			$menu[5][0] = $data['name'];
+			$submenu['edit.php'][5][0] = $data['name'];
+			$submenu['edit.php'][10][0] = $data['add_new'];
+			$submenu['edit.php'][16][0] = __('Tags');
+			echo '';
+		}
+	}
+}
+
+// Callback funtion that changes the label to the new ones
+function wds_change_post_labels() {
+	global $wp_post_types;
+
+	$labels = &$wp_post_types['post']->labels;
+	$menu_icon = &$wp_post_types['post']->menu_icon;
+
+	$data =  get_option('RenamePostLabels');
+
+	$menu_icon = $data['menu_icon'];
+
+	if($data){
+		$labels->name = $data['name'];
+		$labels->singular_name =  $data['singular_name'];
+		$labels->add_new =  $data['add_new'];
+		$labels->add_new_item =  $data['add_new_item'];
+		$labels->edit_item =  $data['edit_item'];
+		$labels->new_item =  $data['new_item'];
+		$labels->view_item =  $data['view_item'];
+		$labels->search_items =  $data['search_items'];
+		$labels->not_found =  $data['not_found'];
+		$labels->not_found_in_trash =  $data['not_found_in_trash'];
+	}
 }
 
 ?>
